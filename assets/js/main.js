be9dcc6a27c9a1eff874efb22dc3d071de5f2b0b
terry-lardner <terryjx@gmail.com>
2016-02-25 22:50:28 +0000

    cross browser compatibility

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,18 @@
 'use strict';
 (function() {
-	const $carousel = $('#carousel');
-	const $carousel1 = $carousel.find('#carousel-item-1');
-	const $carousel2 = $carousel.find('#carousel-item-2');
-
-	const $previewContainer = $('#preview-container');
-	const $previewContent = $previewContainer.find('.preview-content');
-	const $overlay1 = $previewContent.find('.preview-overlay-1');
-	const $overlay2 = $previewContent.find('.preview-overlay-2');
-	const $overlay3 = $previewContent.find('.preview-overlay-3');
-	const $overlay4 = $previewContent.find('.preview-overlay-4');
+	const $carousel = $('#carousel'),
+	$previewContainer = $('#preview-container'),
+	$carousel1 = $carousel.find('#carousel-item-1'),
+	$carousel2 = $carousel.find('#carousel-item-2'),
+	$previewContent = $previewContainer.find('.preview-content'),
+	$overlay1 = $previewContent.find('.preview-overlay-1'),
+	$overlay2 = $previewContent.find('.preview-overlay-2'),
+	$overlay3 = $previewContent.find('.preview-overlay-3'),
+	$overlay4 = $previewContent.find('.preview-overlay-4');
 
 	setInterval(function(){
 		$carousel1.toggleClass('transparent');
-	}, 5000);
+	}, 6000);
 
 
 	//Event listeners
